1731f910fa1f414083816ffb5cc712593831716a
‘Kalophain14’ <chueutemosho@gmail.com>
2026-01-05 21:06:49 +0200

    FirstJava Note

---
+++ 01 - Introduction/FirstJavaProgramUsingNotepad.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+class FirstJavaProgramUsingNotepad
+{
+    public static void main(String args[]) //method
+    {
+        System.out.println("Java is awesome");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+    }
+}
