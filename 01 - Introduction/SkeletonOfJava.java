e1980c2f8e0e20cc1ab9b50649f0fcc3078e5602
‘Kalophain14’ <chueutemosho@gmail.com>
2026-01-06 22:33:57 +0200

    JavaSkeletons

--- 01 - Introduction/SkeletonOfJava.java
+++ 01 - Introduction/SkeletonOfJava.java
@@ -7,3 +7,13 @@ class SkeletonOfJava
         System.out.println("Hello World");
     }
 }
+
+// java.lang = is a package
+// class should be same as file name although can be different
+// inside is the class is a method called "main"
+// main method should always be void "void main"
+// public is for the function to be found by java outside
+// static allows the use of the class without creating the object of the class
+// when compiling java calls out the function which includes the main
+// strings args[] can choose to add arguements or not
+//printerln is a method it prints
