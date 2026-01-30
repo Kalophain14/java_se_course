ddebd24da4bd242e6df79920a2d63b5629acee2e
Kalophain14 <chueutemosho@gmail.com>
2026-01-30 22:31:58 +0200

    firstjavaprogram

--- 01 - Introduction/Instructor.java
+++ 01 - Introduction/Instructor.java
@@ -1,6 +1,10 @@
 // First program
 // JVM calls main method which is the function of the class
-// writing static helps make it easier to call it
+// the compiler generate a byte code called class using javac
+// main = The main method it should always be void
+// public = it means it is accessible
+// static = helps you call it without creating the objects
+// (String[] args) = common line arguements they are optional whether you pass arguements or not
 
 public class Instructor{
     public static void main(String[] args){
