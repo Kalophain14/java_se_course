be2d2f3fc407f864391d336e610970f8502216c7
Kalophain14 <chueutemosho@gmail.com>
2026-02-01 21:15:24 +0200

    Checking ranges of the data types

--- 02 - Data Types - Variables and Literals/CheckSizeRange.java
+++ 02 - Data Types - Variables and Literals/CheckSizeRange.java
@@ -1,8 +1,29 @@
 public class CheckSizeRange{
     public static void main(String[] args)
     {
+        System.out.println("Sizes of Integer :");
         System.out.println("Int Min: "+ Integer.MIN_VALUE);
         System.out.println("Int Max: "+ Integer.MAX_VALUE);
         System.out.println("Int Bytes: "+ Integer.BYTES);
+
+        System.out.println("Sizes of Float :");
+        System.out.println("Int Min: "+ Float.MIN_VALUE);
+        System.out.println("Int Max: "+ Float.MAX_VALUE);
+        System.out.println("Int Bytes: "+ Float.BYTES);
+
+        System.out.println("Sizes of Byte :");
+        System.out.println("Int Min: "+ Byte.MIN_VALUE);
+        System.out.println("Int Max: "+ Byte.MAX_VALUE);
+        System.out.println("Int Bytes: "+ Byte.BYTES);
+
+        System.out.println("Sizes of Long :");
+        System.out.println("Int Min: "+ Long.MIN_VALUE);
+        System.out.println("Int Max: "+ Long.MAX_VALUE);
+        System.out.println("Int Bytes: "+ Long.BYTES);
+
+        System.out.println("Sizes of Double :");
+        System.out.println("Int Min: "+ Double.MIN_VALUE);
+        System.out.println("Int Max: "+ Double.MAX_VALUE);
+        System.out.println("Int Bytes: "+ Double.BYTES);
     }
 }
