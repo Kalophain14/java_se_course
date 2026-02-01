654cb61bbc6dbbf58290d64d05651627f513709a
Kalophain14 <chueutemosho@gmail.com>
2026-02-01 20:57:04 +0200

    Introduction into data type and variable literals

---
+++ 02 - Data Types - Variables and Literals/DataTypes.java
@@ -0,0 +1,18 @@
+/* Data Types
+
+        Primitive DataTypes Catogary
+Intergral = byte, short, int and long
+Floating point = float and double
+char
+boolean = true or false
+
+Sizes and   range
+ byte = 1   | - 128 & 127
+short = 2   | -32768 to 32767
+int = 4     | - 2147483648 | 2147484648
+long = 8    | -
+float 4     |
+double = 8  |
+char = 2    | 0 to 65535 Unicode aka Ascii
+boolean =   | true / false
+ */
