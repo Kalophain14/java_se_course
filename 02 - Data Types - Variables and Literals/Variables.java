26b5bf415442a4541e3ae9faf5407ac9cd451fff
Kalophain14 <chueutemosho@gmail.com>
2026-02-02 20:52:34 +0200

    Declaration of variables

---
+++ 02 - Data Types - Variables and Literals/Variables.java
@@ -0,0 +1,18 @@
+/* What are variables?
+ */
+
+public class Variables
+{
+    public static void main(String [] args)
+    {
+        byte b = 2;
+        int a = 1000;
+        float f = 23.5f;
+        char c = 'A';
+
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(f);
+        System.out.println(c);
+    }
+}
