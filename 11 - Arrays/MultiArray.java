68c0a1dbfcbbd8f9f3d9585ba4940401bb74f85b
Kalophain14 <chueutemosho@gmail.com>
2026-02-05 22:07:01 +0200

    arrays are better

---
+++ 11 - Arrays/MultiArray.java
@@ -0,0 +1,9 @@
+public class MultiArray
+{
+    public static void main(String[] args)
+    {
+        int [][] sample = {{1, 2, 3}, {4, 5, 6}};
+        int x = sample[0][2];
+        System.out.println(x);
+    }
+}
