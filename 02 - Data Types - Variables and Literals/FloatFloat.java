2f11129c8685760e6069985355fb5ce9be1f2173
Kalophain14 <chueutemosho@gmail.com>
2026-02-06 19:24:15 +0200

    IntegralData and Floatation

---
+++ 02 - Data Types - Variables and Literals/FloatFloat.java
@@ -0,0 +1,20 @@
+/*
+// Float = takes 4 bytes
+e.g 12.53946 (float will take all the 6 values)
+If its more take double which takes 8 more
+
+163.2
+163.2 *100/100 = 163.52
+
+16352 * 10^-2
+
+// Charectors aka ASCII
+Char only takes positive numbers
+Ascii code reaches from 0 - 127
+
+A = 65 Z = 90
+a = 97 z = 122
+Special characters + * , c
+
+//Unicode = Languages
+ */
