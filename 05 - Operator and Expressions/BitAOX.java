801ed1a42bc494d12e96dfee2e469de495d0f2b3
‘Kalophain14’ <chueutemosho@gmail.com>
2026-02-10 20:01:52 +0200

    Bitwise Operators

---
+++ 05 - Operator and Expressions/BitAOX.java
@@ -0,0 +1,25 @@
+/*
+Bitwise operators = they can only perfomed on integer values (doesnt work on float and double)
+
+AND & = Both values has to be true otherwise false
+OR | = if any value is true then it is true
+NOT ~ =
+XOR (exclusive or) ^ = if any of the two values are different it is true
+RIGHT SHIFT >>
+UNSIGNED RIGHT SHIFT >>>
+LEFT SHIFT <<
+
+int x = 10, y = 6, 3;
+
+// Binary form example
+x = 0 0 0 0 1 0 1 0
+y = 0 0 0 0 1 1 1 0
+
+result = x & y (AND OPERATORS)
+r = 0 0 0 0 0 0 1 0
+
+result2 = x | y (OR OPERATORS)
+r2 =
+
+
+ */
