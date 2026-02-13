398bc45fe5a1f10b1a4649e84be4bc8c2d393d8c
‘Kalophain14’ <chueutemosho@gmail.com>
2026-02-13 20:20:09 +0200

    update git ignore

---
+++ 05 - Operator and Expressions/MaskingMerging.java
@@ -0,0 +1,16 @@
+/*
+byte a = 0;
+
+
+a = 8 7 6 5 4 3 2 1 0
+every binary bit we can make some meaning for e.g
+
+0 = no, false, absent
+1 = yes, true, present
+
+b = 128 64 32 16  8  4  2 1
+a = 0   0  0  0   0  0  0 0
+
+0 3 6 9
+
+*/
