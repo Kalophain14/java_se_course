7a4a17b1a2f4875f9cba417824469b0ebb23b0d6
‘Kalophain14’ <chueutemosho@gmail.com>
2026-02-16 21:08:00 +0200

    String and concatinating

---
+++ 06 - String Class and Printing/Printing.java
@@ -0,0 +1,23 @@
+/*
+printing
+
+
+System.out.print()
+System.out.println() = print in line
+System.out.printf() = has specifiers e.g "%d, %s %f"
+System.out.format() = for creating different format
+*/
+
+public class Printing {
+    public static void main(String[] args) {
+        /*
+         int a = 10;
+         float b = 112.3f;
+         char c = 'a';
+         String str = "abc";
+         System.out.println(str);
+        */
+        int x = 10, y = 20;
+        System.out.println("sum of "+x+" and "+y+" is "+(x + y));
+    }
+}
