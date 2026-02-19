1666f78a2927d1528454a0ca0c599cfa21e4611a
‘Kalophain14’ <chueutemosho@gmail.com>
2026-02-19 21:54:34 +0200

    Strings equals

--- 06 - String Class and Printing/StringMeth.java
+++ 06 - String Class and Printing/StringMeth.java
@@ -8,24 +8,20 @@ trim() > removes the blank spaces in a string (best used when user gives input)
 substring(int begin) > it prints from starting index
 substring(int begin, int end) > it prints from beginning and ending index (3, 6)
 replace(char old, char new) > it replaces/modified the letter chosen to another letter
-valueOf(int i) > 
+valueOf(int i) >
 
 *boolean Methods*
-startsWith(String s)
-endsWith(String s)
-equals(String s)
-equalsIgnoreCase(String s)
+startsWith(String s) > True / false whether a string starts with something
+endsWith(String s) > True / false whether a string ends with something
+equals(String s) > whether a string is equal to another string
+equalsIgnoreCase(String s) > whether is equal to another string by ignore the Case sensetive
 
 *Character Methods*
-charAt(int index)
+charAt(int index) > searches where a char is at in the index
 
 *int Methods*
-length()
-indexOf(String s)
-lastIndexOf(String s)
-compareTo(Strings s)
-
-Part 2
-
-Part 3
+length() > prints out the length of string
+indexOf(String s/char) > searches for the string/char and returns its index
+lastIndexOf(String s) > searches for the last index right to left direction
+compareTo(Strings s) >
  */
