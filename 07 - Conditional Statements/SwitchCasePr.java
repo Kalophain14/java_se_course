0ef0fea84b808002e1a9b9cd1245ca6ab3c28dfe
Temoso <chueutemosho@gmail.com>
2026-03-04 20:39:12 +0200

    feat: explore switch case fundamentals
    - SwitchCases vs If Else statement
    - The importance of breaks
    - Exercises to understand the usage of dealing with cases

--- 07 - Conditional Statements/SwitchCasePr.java
+++ 07 - Conditional Statements/SwitchCasePr.java
@@ -1,4 +1,6 @@
- public class SwitchCasePr {
+import java.util.Scanner;
+
+public class SwitchCasePr {
     public static void main(String[] args)
     {
         String n = "1";
@@ -27,5 +29,31 @@
             default:
                 System.out.println("Not valid");
         }
+
+        //domain exercise
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a domain"); //asking user to enter their domain
+        String domain = scan.nextLine(); //storing the domain
+
+        String ext = domain.substring(domain.lastIndexOf(".")+1); //gives out the string starting from . going backwards also other characters
+
+        switch(ext){
+            case "com":
+                System.out.println("Commercial");
+                break;
+            case "org":
+                System.out.println("Organisation");
+                break;
+            case "gov":
+                System.out.println("Government");
+                break;
+            case "net":
+                System.out.println("Network");
+                break;
+            default:
+                System.out.println("Invalid domain");
+        }
+
     }
 }
