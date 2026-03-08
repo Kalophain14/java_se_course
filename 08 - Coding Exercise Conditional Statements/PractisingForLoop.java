cf61f065d4bbe498b29f36bec14f8ede8942541c
Temoso <46405512+Kalophain14@users.noreply.github.com>
2026-03-08 20:35:22 +0200

    Feat: Exercises for a for loop, practicing for loop

--- 08 - Coding Exercise Conditional Statements/PractisingForLoop.java
+++ 08 - Coding Exercise Conditional Statements/PractisingForLoop.java
@@ -6,12 +6,31 @@
  *    1. Print squares of numbers 1 to 10
  *    2. Print multiplication table for any number
  *    3. Sum of all even numbers from 1 to 50
- *    4. Print factors of a given number
- *    5. Check if a number is prime using for loop
  * ============================================================
  */
 public class PractisingForLoop {
 
     public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+        // Print squares
+        for (int s = 1; s <= 10; s++) {
+            System.out.println("1² = 1");
+        }
+
+        // print multiplication
+        int tableNumber = 7;
+        for (int t = 1; t <= 10; t++) {
+            System.out.println(tableNumber + " x " + t + " = " + (tableNumber * t));
+        }
+        //sumOfAll
+        int evenSum = 0;
+        for (int e = 1; e <= 50; e++){
+            if (e % 2 == 0) {
+                evenSum += e;
+            }
+        }
+        System.out.println("sum of all that number is " + evenSum);
     }
 }
