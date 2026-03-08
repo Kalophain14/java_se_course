0d8b9b2dbb0ad4c2d5e40983f313e885b99c3333
Temoso <chueutemosho@gmail.com>
2026-03-08 20:09:56 +0200

    Feat: Learning about for loops, when to use them and how to use them, the importance of iteration

--- 08 - Coding Exercise Conditional Statements/ForLoop.java
+++ 08 - Coding Exercise Conditional Statements/ForLoop.java
@@ -30,14 +30,23 @@
  *
  *  SYNTAX:
  *    for (initialisation; condition; update) {
- *        // statements
+ *              1              2        3
+ *        // statements 4
+ *        // process repeats again and again until the condition is true
+ *        // then stops
  *    }
  *
- *  USE WHEN: number of iterations is known in advance
+ *  USE WHEN:
+ *  number of iterations is known in advance
+ *  For this many times you know how  you're going to repeat
+ *  e.g. making a tea you know how much sugar of teaspoons you want
  * ============================================================
  */
 public class ForLoop {
 
     public static void main(String[] args) {
+        for (int i = 10; i > 0; i-=1){
+            System.out.println(i);
+        }
     }
 }
