d74256294f835c298927e7cea407bf461477dd39
Temoso <chueutemosho@gmail.com>
2026-04-09 14:40:10 +0200

    Learned: Student Challenge - How to access an array using for loop and for each loop - for each loop easy to access an array only forward direction for(i : num) - For loop used to access forward and backwards in an array - doing tasks

--- 09- Arrays/StudentChallengeFindArray.java
+++ 09- Arrays/StudentChallengeFindArray.java
@@ -1,6 +1,36 @@
+import java.util.Scanner;
+
 public class StudentChallengeFindArray {
 
     public static void main(String[] args) {
-        // Student Challenge - Find
+        //Student Challenges
+
+        //01 - Finding SUM of all elements
+
+        int num[] = {3, 9, 7, 8, 12, 6, 15, 5, 4, 10};
+
+        int sum = 0;
+
+        for(int i = 0; i < num.length; i++) {
+            sum = sum + num[i]; //storing the addition data into sum
+        }
+        System.out.println("sum of all the data is: " + sum);
+
+
+        //02 - Searching through an element
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your key");
+
+        int key = sc.nextInt();
+
+
+        for(int a = 0; a < num.length; a++) {
+            if(num[a] == key){
+                System.out.println(a);
+                System.exit(0); //exiting the task after a key is found
+            }
+        }
+        System.out.println("Not found");
     }
 }
