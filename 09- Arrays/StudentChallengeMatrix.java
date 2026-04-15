bcd5f62cf47a9ec7dfe4bf582745582f0dda5ef6
Temoso <46405512+Kalophain14@users.noreply.github.com>
2026-04-16 00:03:30 +0200

    feat: Challanges
    - adding 2 Matrix

--- 09- Arrays/StudentChallengeMatrix.java
+++ 09- Arrays/StudentChallengeMatrix.java
@@ -1,6 +1,31 @@
 public class StudentChallengeMatrix {
 
-    public static void main(String[] args) {
-        // Student Challenge - Matrix
+    public static void main(String[] args)
+    {
+
+        // Adding 2 Matrix
+        int A[][] = {{3,5,9},{7,6,2},{4,3,5}};
+        int B[][] = {{1,5,2},{6,8,4},{3,9,7}};
+
+        int C[][] = new int[3][3];
+
+        for(int i = 0; i< A.length; i++)
+        {
+            for (int j = 0; j < A[0].length; j++)
+            {//Adding
+                C[i][j] = A[i][j] + B[i][j];
+            }
+        }
+        for(int x[]:C)
+        {
+            for(int y:x)
+            {
+                System.out.print(y+" ");
+            }
+            System.out.println("");
+        }
+
+        //Multiplying 2 Matrix
+
     }
 }
