5fd6c7b193028d79b6247de42386bd9705fcdd6a
Temoso <chueutemosho@gmail.com>
2026-04-15 23:33:55 +0200

    feat:
    Initializing a 2D Array
    Accessing using a for loop
    Accessing Using a for each loop
    
    Jagged array - an array of arrays where each row can have a different number of columns — unlike a regular 2D array where every row is the same size.

--- 09- Arrays/PractisingTwoDimensional.java
+++ 09- Arrays/PractisingTwoDimensional.java
@@ -2,5 +2,12 @@ public class PractisingTwoDimensional {
 
     public static void main(String[] args) {
         // Practising Two-Dimensional Arrays
+
+        //int A[][] = new[5][5];
+
+        int B[][] = {{1,2,3},{2,4,6},{1,3,5}};
+
+        System.out.println(B.length);
+
     }
 }
