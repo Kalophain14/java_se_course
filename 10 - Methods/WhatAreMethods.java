9e91fa93d9f4c960d1f334ca54fa5c2f2b977321
Temoso <chueutemosho@gmail.com>
2026-04-17 19:58:19 +0200

    feat:
    - Understanding how methods are created normally called functions in other languages
    - They are created to help re-use the same code again and again without creating the same code multiple times
    - It uses parameters and has a return type of a datatype

--- 10 - Methods/WhatAreMethods.java
+++ 10 - Methods/WhatAreMethods.java
@@ -1,6 +1,35 @@
+/*
+What Are Methods?
+- They are like functions
+- They are members of a class that gives functionality to the class e.g
+length(), indexOf(), equals(), Substring()
+- They can also return a void, integer, string etc
+
+Skeleton of a method
+returnType methodName (Parameters List){
+      //Signature of a method
+      //return something
+}
+*/
+
+
 public class WhatAreMethods {
 
+    //Example method that returns a larger num
+
+    //Definition of a Method
+    static int maximum(int x, int y)
+    {
+        if (x > y)
+            return x;
+        else
+            return y;
+    }
+    //Calling the Method
     public static void main(String[] args) {
-        // What Are Methods
+
+        int a = 20, b = 150, c;
+        c = maximum(a,b);
+        System.out.println(c);
     }
 }
