4486b8b3143ae942c8fd15a777cdc4342b40bbda
Temoso <46405512+Kalophain14@users.noreply.github.com>
2026-04-18 22:43:42 +0200

    feat:
    - Understanding how methods are created normally called functions in other languages
    - They are created to help re-use the same code again and again without creating the same code multiple times
    - It uses parameters and has a return type of a datatype
    - Calling method from parameters
    - Passing parameters Primitive using factual data and String using object referencing they are all the same they dont create a new object they reference or call from factual to actual

--- 10 - Methods/ParameterPassingInJava.java
+++ 10 - Methods/ParameterPassingInJava.java
@@ -1,6 +1,33 @@
 public class ParameterPassingInJava {
 
+    /*Primitive passing by value
+    * int, double, boolean, short
+    * copies the actual value into the method parameters
+    * */
+    static int add(int x, int y) {
+        int z;
+        z = x + y;
+        return z;
+    }
+
+    /*String/Objects Method - passing by object referencing
+    * When you pass a String or any Object
+    * Java passes memory address aka reference of the object
+    * */
+    static String welcome(String n) {
+        return "Welcome Mr/Miss " + n;
+    }
+
     public static void main(String[] args) {
-        // Parameter Passing in Java
+
+        /*Primitive Method Calling*/
+        int a = 10, b = 5, c;
+        c = add(a, b);
+        System.out.println(c); // prints 15
+
+        /*String Passing Calling*/
+        String name = "Victor";
+        String message = welcome(name);
+        System.out.println(message); // prints Welcome Mr/Miss Victor
     }
 }
