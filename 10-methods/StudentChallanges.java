37055e00ab14aa4ab9d7bf25956a400414a20785
‘Kalophain14’ <chueutemosho@gmail.com>
2026-04-30 22:53:54 +0200

    feat:
    -  program that finds the highest number of the two

--- 10-methods/StudentChallanges.java
+++ 10-methods/StudentChallanges.java
@@ -10,15 +10,35 @@ public class StudentChallanges
         } return true;
     }
 
+    /*Find HighestCommonNumbers of 2 numbers*/
+
+    static int HCG(int m, int n)
+    {
+       while(m!=n){
+           if (m>n)
+           {
+               m=m-n;
+           }
+           else {
+               n=n-m;
+           }
+       }
+       return n;
+    }
+
     /* Using the methods*/
     public static void main(String[] args)
     {
+        //PrimeMethod Calling
         if(isPrime(100)){
             System.out.println("prime Number");
         }
         else{
             System.out.println("not a prime Number");
         }
+
+        //HCG MethodCalling
+        System.out.println("The highest number is: " + HCG(50,15));
     }
 }
 
