c9347daa1a5728697fc3044d16a3affb645553a7
‘Kalophain14’ <chueutemosho@gmail.com>
2026-05-04 21:15:48 +0200

    feat:
    -  Method overloading is when you use multiple parameters in a method or can be in the same method names etc

--- 10-methods/MethodOverloading.java
+++ 10-methods/MethodOverloading.java
@@ -1,6 +1,27 @@
 public class MethodOverloading {
 
-    public static void main(String[] args) {
-        // Method Overloading
+    //method overloading
+    static int max(int x, int y) {
+        return x > y ? x : y;
     }
+
+    static float max(float x, float y) {
+        return x > y ? x : y;
+    }
+
+    static int max(int x, int y, int z) {
+        return x > y ? (x > z ? x : z) : (y > z ? y : z);
+    }
+
+    public static void main(String[] args) {
+        /*
+         Method Overloading
+         You can have the same names on the method as long the difference
+         - can be the parameter list (int, float etc)
+         - data types int max(int x, int y) float(int x, int y)
+         You can have as many as parameters as you want
+         */
+
+            System.out.println(max(70, 39, 55));
+        }
 }
