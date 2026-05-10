ec41793e0b8952971d5e1ab46bbba69068a56029
‘Kalophain14’ <chueutemosho@gmail.com>
2026-05-10 22:50:44 +0200

    feat:
    Variables Updating

--- 02-data-types-variables-and-literals/Variables.java
+++ 02-data-types-variables-and-literals/Variables.java
@@ -1,18 +1,52 @@
 /* What are variables?
  */
 
-public class Variables
-{
-    public static void main(String [] args)
-    {
-        byte b = 2;
-        int a = 1000;
-        float f = 23.5f;
-        char c = 'A';
+public class Variables {
+    public static void main(String[] args) {
 
-        System.out.println(b);
-        System.out.println(a);
-        System.out.println(f);
-        System.out.println(c);
+        // ❎ variable = A reusable container for a value.
+        //               A variable behaves as if it was the value it contains.
+
+        // 🟥 Primitive = simple value stored directly in memory (stack)
+        // 🟦 Reference = memory address (stack) that points to the (heap)
+
+        // 🟥 Primitive vs 🟦 Reference
+        //    ---------                      ---------
+        //    int                           string
+        //    double                    array
+        //    char                        object
+        //    boolean
+
+        int age = 21;
+        int year = 2025;
+        int quantity = 1;
+
+        double price = 19.99;
+        double gpa = 3.5;
+        double temperature = -12.5;
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        boolean isStudent = true;
+        boolean forSale = false;
+        boolean isOnline = true;
+
+        String name = "Bro Code";
+        String food = "pizza";
+        String email = "fake123@gmail.com";
+        String car = "Mustang";
+        String color = "red";
+
+        System.out.println("Your choice is a " + color + " " + year + " " + car);
+        System.out.println("The price is: " + currency + price);
+
+        if(forSale){
+            System.out.println("There is a " + car + " for sale");
+        }
+        else{
+            System.out.println("The " + car + " is not for sale");
+        }
     }
 }
