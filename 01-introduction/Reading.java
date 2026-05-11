f20905bf73b130fddc94de8d68d012cbeaf618bb
‘Kalophain14’ <chueutemosho@gmail.com>
2026-05-11 20:09:29 +0200

    feat:
    Closing a scanner and boolean next

--- 01-introduction/Reading.java
+++ 01-introduction/Reading.java
@@ -43,10 +43,18 @@ public class Reading{
         System.out.print("What is your GPA: ");
         double gpa = Double.parseDouble(sc.nextLine()); // reads as String, parses safely
 
+        System.out.print("Are you a student? (True/False)");
+        boolean isStudent = sc.nextBoolean();
 
         System.out.println("Hello " + name);
         System.out.println("You are " + age + " years old");
         System.out.println("You're GPA score is " + gpa);
+
+        if(isStudent){
+            System.out.println("You're a student");
+        } else{
+            System.out.println("You're not a student");
+        }
 
         sc.close();
     }
