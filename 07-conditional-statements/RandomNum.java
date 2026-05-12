d7c8992a3c99f06f445c1ebffb17db75659bf0ba
‘Kalophain14’ <chueutemosho@gmail.com>
2026-05-12 18:26:06 +0200

    generating random numbers using random import creating a random heads and tail game

--- 07-conditional-statements/RandomNum.java
+++ 07-conditional-statements/RandomNum.java
@@ -7,8 +7,8 @@ public class RandomNum {
         Random random = new Random();
         
         int num;
-        num = random.nextInt(1, 3);
-        //inputting random numbers the last number 3 doesn't get included
+        num = random.nextInt(1, 6);
+        //inputting random numbers the last number 6 doesn't get included
         System.out.println(num);
 
         boolean isHeads;
