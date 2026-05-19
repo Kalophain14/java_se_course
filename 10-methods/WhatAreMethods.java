36bb9b63fafde5e90c9a44f5af092d916043a388
‘Kalophain14’ <chueutemosho@gmail.com>
2026-05-19 19:47:22 +0200

    Different types of methods prints, print with input && takes input, return value, store value

--- 10-methods/WhatAreMethods.java
+++ 10-methods/WhatAreMethods.java
@@ -19,10 +19,56 @@ returnType methodName (Parameters List){
 */
 
 
-public class WhatAreMethods {
+import java.util.Scanner;
 
-    //Definition of a Method
-    //Formal parameters only change but they dont affect actual parameters
+public class WhatAreMethods {
+    //Calling the Method
+    public static void main(String[] args) {
+        System.out.println("===Method Calling===");
+
+        //Methods that prints Call
+        happyBirthday();
+
+        //Methods input Print Call
+        int age =  30;
+        String name = "Khensani";
+        birthdayGirl(name, age);
+
+        //Methods takes input and return value stored
+        int a = 150; int b = 200;
+        System.out.println("Maximum Number is " + maximum(a,b));
+
+        //GettingFulName
+        String fullName = getFullName("Khensani", "Simelani");
+        System.out.println(fullName);
+
+        //AgeMethod Call
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        age = sc.nextInt();
+
+        if(ageCheck(age)){
+            System.out.println("You can enter groove");
+        } else  {
+            System.out.println("You are young go home to your family");
+        }
+    }
+
+    /*
+    Definition of a Method
+    Formal parameters only change but they dont affect actual parameters
+    */
+    //Method that only prints
+    static void happyBirthday(){
+        System.out.println("Happy Birthday");
+    }
+
+    //Methods that takes input and print value
+    static void birthdayGirl(String name, int age){
+        System.out.printf("Happy Birthday %s you are now %d years old\n", name, age);
+    }
+
+    //Methods that takes input as arguments and return value + value to be stored
     static int maximum(int x, int y)
     {
         if (x > y)
@@ -31,27 +77,14 @@ public class WhatAreMethods {
             return y;
     }
 
-    static void happyBirthday(){
-        System.out.println("Happy Birthday");
+    static String getFullName(String first, String last){
+        return first + " " + last;
     }
 
-    //Calling the Method
-    public static void main(String[] args) {
-        System.out.println("===Maximum Method===");
-
-        //Option 1
-        int a = 20, b = 150, c;
-        c = maximum(a,b); //actual parameters
-        System.out.println("Max Number:" + c);
-
-        //Option 2
-        System.out.println(maximum(a,b));
-
-        //Option 3 Using a constructor as an object
-        WhatAreMethods wme = new WhatAreMethods();
-        System.out.println(wme.maximum(a,b));
-
-        happyBirthday();
-
+    static boolean ageCheck(int age){
+        if (age > 21 && age < 50)
+            return true;
+        else
+            return false;
     }
 }
