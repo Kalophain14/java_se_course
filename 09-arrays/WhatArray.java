4a53df2538469d63f8d5bd57ac8c28fdfd094c72
‘Kalophain14’ <chueutemosho@gmail.com>
2026-05-25 20:12:59 +0200

    Enter user data using arrays, a small food program using for loop and for each

--- 09-arrays/WhatArray.java
+++ 09-arrays/WhatArray.java
@@ -5,10 +5,11 @@ Array = a collection of values of the same data type
  */
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class WhatArray {
     public static void main(String[] args) {
-        String[] fruits = {"Apple", "Banana", "Kiwi", "Orange"};
+        /* String[] fruits = {"Apple", "Banana", "Kiwi", "Orange"};
 
         //fruits [0] = "Pinapple"; //Changing value inside an array
         //int numFruits = fruits.length; //Getting the length
@@ -26,6 +27,26 @@ public class WhatArray {
         //For Each Loop
         for(String fruit : fruits) {
             System.out.println(fruit);
+        }
+        */
+
+        Scanner sc = new Scanner(System.in);
+        String [] foods = new String[3];
+        int size;
+
+        System.out.print("What number of food do you want?: ");
+        size = sc.nextInt();
+        sc.nextLine();
+
+        foods = new String[size]; //User sets number
+
+        for (int i = 0; i < foods.length; i++) {
+            System.out.println("Enter your favourite food: ");
+            foods[i] = sc.nextLine();
+        }
+
+        for (String food : foods) {
+            System.out.println(food);
         }
     }
 }
