bb3f2f935a1005180bc5109c5145b0dd08285b2b
‘Kalophain14’ <chueutemosho@gmail.com>
2026-05-26 20:16:36 +0200

    The importance of VarArg how they accept differnt arguements of arrays using the ellipsis (...)

---
+++ 09-arrays/VarArgs.java
@@ -0,0 +1,22 @@
+public class VarArgs {
+    public static void main(String[] args) {
+        /*
+        Vararguments = allow a method to accept a varying # of arguements
+        makes method more flexible, no need for overloaded methods
+        Java will pack arguements into an array
+        (ellipsis)
+         */
+
+        double result = average(1, 2, 3, 4, 6, 7, 8, 9, 10);
+        System.out.printf("The Average of this numbers is: %.2f ", result);
+    }
+
+    static double average (double ... numbers){ //the .... indicates ellipsis array
+        double sum = 0;
+
+        for(double number : numbers){
+            sum += number;
+        }
+        return sum / numbers.length;
+    }
+}
