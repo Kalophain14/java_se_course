8e29e2936054a8a27a5d7aa91965dfc64edc5897
‘Kalophain14’ <chueutemosho@gmail.com>
2026-06-02 20:18:05 +0200

    added more methods for objects use case

--- 11-object-oriented-programming/Objects.java
+++ 11-object-oriented-programming/Objects.java
@@ -6,22 +6,28 @@ public class Objects {
         Object = an entity that holds (attributes) //variable name, food etc
                  and can perform actions(methods) sleep(), eat()
                  It is a reference data type
+                 Using the new keyword as a reference pointer
+                 .(attributes) let u access the attributes of the methods in constructor
          */
         
         Car car = new Car(); //use case of using a method as an object
-        Car car2 = new Car();
 
+        /*
         car.isRunning = true; //you can modify within the running
 
         System.out.println(car.model);
         System.out.println(car.year);
-        System.out.println(car.price);
-        System.out.println(car.isRunning);
+        System.out.println("The vehicle price is " + car.price);
+        System.out.println("The car is now running? " + car.isRunning);
         System.out.println(car.color);
+        */
 
         //Running the methods within the methods
+        System.out.println(car.isRunning);
         car.run();
+        System.out.println(car.isRunning);
         car.stop();
+        System.out.println(car.isRunning);
         car.drive();
     }
 }
