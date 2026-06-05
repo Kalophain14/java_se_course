fb858bf2f766fde12c19149803e44450fe6b702a
‘Kalophain14’ <chueutemosho@gmail.com>
2026-06-05 21:25:36 +0200

    Method recursion a method that calls itself until the program stops

--- 10-methods/Recursion.java
+++ 10-methods/Recursion.java
@@ -1,6 +1,29 @@
 public class Recursion {
 
+    //reverse recursion
+    static void fun(int n){
+        if(n>0){
+            System.out.println("You're fun rec is: "+ n);
+            fun(n-1);
+        }
+    }
+
+    //forward recursion
+    static void fun2(int n){
+        if(n>0){
+            fun2(n-1);
+            System.out.println("You're sec rec num is: "+ n);
+        }
+    }
+
+
     public static void main(String[] args) {
-        // Recursion
+        /*
+        Recursion Method = is a method that calls itself until the logic is false
+         */
+
+        //Calling recursion method
+        fun(3);
+        fun2(3);
     }
 }
--- 11-object-oriented-programming/PrinciplesOfOOP.java
+++ 11-object-oriented-programming/PrinciplesOfOOP.java
@@ -1,6 +1,12 @@
 public class PrinciplesOfOOP {
 
     public static void main(String[] args) {
-        // Principles of Object-Oriented Programming
+        /*
+        Principles of Object-Oriented Programming are:
+        Abstraction
+        Encapsulation
+        Inheritance
+        Polymorphism
+        */
     }
 }
