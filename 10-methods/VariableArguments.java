60c3337aeecc79cbc3700657d749ab0cb60843e4
‘Kalophain14’ <chueutemosho@gmail.com>
2026-06-05 22:46:50 +0200

    VAR argument eclipse

--- 10-methods/VariableArguments.java
+++ 10-methods/VariableArguments.java
@@ -1,6 +1,12 @@
 public class VariableArguments {
 
+    static void show(String...S){
+        for (String s : S){
+            System.out.println("Start: " + s);
+        }
+    }
     public static void main(String[] args) {
         // Variable Arguments
+        show("Baby", "Khensani", "Paballo");
     }
 }
