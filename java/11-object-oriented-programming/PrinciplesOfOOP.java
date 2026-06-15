4fc7ed31d7a5e4e32a104346c076520877d8f7d4
‘Kalophain14’ <chueutemosho@gmail.com>
2026-06-16 00:16:34 +0200

    comments update

--- java/11-object-oriented-programming/PrinciplesOfOOP.java
+++ java/11-object-oriented-programming/PrinciplesOfOOP.java
@@ -3,10 +3,17 @@ public class PrinciplesOfOOP {
     public static void main(String[] args) {
         /*
         Principles of Object-Oriented Programming are:
-        Abstraction
-        Encapsulation
-        Inheritance
-        Polymorphism
+
+        Abstraction = Hiding internal details, showing only required details.
+                    - eg TV, Phone you dont know how it works it just works
+                    - You dont know what is going inside, it works
+
+        Encapsulation =
+
+        Inheritance =
+        Polymorphism =
         */
+
+
     }
 }
