547e57501b06231f9c6db90c92dee36a7b6bad1e
‘Kalophain14’ <chueutemosho@gmail.com>
2026-06-16 22:10:32 +0200

    Creating classes using methods, understanding the relationship between the properties(variables) and behaviors(methods) of that class and how to access them using the new keyword

--- java/11-object-oriented-programming/HowToWriteAClass.java
+++ java/11-object-oriented-programming/HowToWriteAClass.java
@@ -1,6 +1,42 @@
 public class HowToWriteAClass {
 
+    //Rectangle Class
+    class Rectangle{
+        //Properties of the method
+        double width;
+        double height;
+
+        //Behaviors as method
+        public Rectangle(double width, double height) {
+            this.width = width;
+            this.height = height;
+        }
+
+
+        //Account Class
+        class Account {
+            //Properties of the class
+            public long accountNumber;
+            public double accountBalance;
+            public string name;
+
+            //behaviors as methods
+            public void deposit(double amount) {
+                accountNumber += amount;
+                accountBalance += amount;
+            }
+            public void withdraw(double amount) {
+                accountNumber -= amount;
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        // How to Write a Class
+        /*
+        How to Write a Class
+
+        Properties as variable of the methods
+        Behaviors as method of inside the class
+         */
     }
 }
