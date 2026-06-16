d18ce2f6f3d53f4cc290caa90461a1965291ec8b
‘Kalophain14’ <chueutemosho@gmail.com>
2026-06-16 21:43:11 +0200

    Class and Objects Properties and Behaviors functions/method as behaviors variables, private variables as properties of the class

--- java/11-object-oriented-programming/ClassVsObject.java
+++ java/11-object-oriented-programming/ClassVsObject.java
@@ -1,6 +1,36 @@
 public class ClassVsObject {
 
-    public static void main(String[] args) {
-        // Class vs Object
+    /*
+    An object is defined by its property and behavior
+
+    Properties = data types, overloading eg car color, wheels etc
+    Behavior = functions, operations you can change them eg can start the car
+
+    //Example
+    - A class will be the engine, the design
+    - An object will be the vehicle
+    - Therefore you can create many objects from a single class
+     */
+
+    class Television {
+       //Properties
+        private int channel;
+        private int volume;
+
+        //behaviours
+        public void changeChannel(int channel) {
+            this.channel = channel;
+        }
+        public void changeVolume(int volume) {
+            this.volume = volume;
+        }
+    }
+
+    class Test {
+        public static void main(String[] args) {
+           Television Tv = new Television();
+
+           Tv.changeChannel(3);
+        }
     }
 }
