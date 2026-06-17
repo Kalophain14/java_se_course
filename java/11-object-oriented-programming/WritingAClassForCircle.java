402f697cc3d4b3de018fe79de96446e437a83852
‘Kalophain14’ <chueutemosho@gmail.com>
2026-06-17 21:53:44 +0200

    creating classes and using objects to build them the importance of the new keyword, you can create multiple objects using the same class == reuse the code, do not repeat yourself

--- java/11-object-oriented-programming/WritingAClassForCircle.java
+++ java/11-object-oriented-programming/WritingAClassForCircle.java
@@ -1,8 +1,6 @@
-
 public class WritingAClassForCircle {
 
-    //Properties
-    static double radius;
+    double radius;
 
     //Methods
     public double area(){
@@ -15,14 +13,23 @@ public class WritingAClassForCircle {
         return 2 * radius * Math.PI;
     }
 
-
     public static void main(String[] args) {
         //calling out the method
-
-        WritingAClassForCircle.radius = 7;
         WritingAClassForCircle obj = new WritingAClassForCircle();
+        WritingAClassForCircle obj2 = new WritingAClassForCircle();
+
+        //Properties
+       obj.radius = 7;
+       obj2.radius = 14;
+
         System.out.println("Area is: " + obj.area());
         System.out.println("Perimeter: " + obj.perimeter());
         System.out.println("circumference: " + obj.circumference());
+
+        System.out.println("==new object==");
+
+        System.out.println("Area2 is: " + obj2.area());
+        System.out.println("Perimeter2: " + obj2.perimeter());
+        System.out.println("circumference2: " + obj2.circumference());
     }
 }
