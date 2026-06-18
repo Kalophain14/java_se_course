f7ceea81ba620c147d83abe65e33570dfa07de5a
‘Kalophain14’ <chueutemosho@gmail.com>
2026-06-18 19:30:16 +0200

    Practicing creating a class of rectangle that has its two properties and two methods that calculate area + perimeter+ added a method that checks whether length is equal to width

--- java/11-object-oriented-programming/StudentChallengeWriteClass2.java
+++ java/11-object-oriented-programming/StudentChallengeWriteClass2.java
@@ -13,21 +13,30 @@ public class StudentChallengeWriteClass2 {
         return  2 * (length + width);
     }
 
+    boolean isSquare(){
+        if(length == width )
+            return true;
+         else
+             return false;
+    }
+
     public static void main(String[] args) {
         // Student Challenge - Write a Class Rectangle
         StudentChallengeWriteClass2 obj = new StudentChallengeWriteClass2();
         StudentChallengeWriteClass2 obj2 = new StudentChallengeWriteClass2();
 
-        obj.length = 10;
-        obj.width = 12;
+        obj.length = 10.2;
+        obj.width = 12.3;
 
-        obj2.length = 4;
-        obj2.width = 10;
+        obj2.length = 4.2;
+        obj2.width = 10.2;
 
         System.out.println("The area of a rectangle: " + obj.area());
         System.out.println("The area of a rectangle: " + obj.perimeter());
+        System.out.println("Lenghth "+ obj.isSquare());
 
         System.out.println("The area of a rectangle: " + obj2.area());
         System.out.println("The area of a rectangle: " + obj2.perimeter());
+        System.out.println("Lenghth "+ obj2.isSquare());
     }
 }
