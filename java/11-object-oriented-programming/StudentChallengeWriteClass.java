833712ba1cb4e000f0096348ce9906c65d1304f8
‘Kalophain14’ <chueutemosho@gmail.com>
2026-06-18 22:45:58 +0200

    Writing a class for student that takes in students course, marks, average score and added a friendly user enter their own score using scanner, created multiple methods using properties that are re-used by objects

--- java/11-object-oriented-programming/StudentChallengeWriteClass.java
+++ java/11-object-oriented-programming/StudentChallengeWriteClass.java
@@ -1,26 +1,70 @@
+import java.util.Scanner;
+
 public class StudentChallengeWriteClass {
 
+    //Properties Student
+    int roll;
     String name;
-    String grade;
-    int age;
+    String course;
+    int mark_1,mark_2,mark_3;
 
-    StudentChallengeWriteClass(String name, String grade, int age){
+
+    /*
+    Constructor
+    StudentChallengeWriteClass(String roll, String name, String grade, int age){
+        this.roll;
         this.name = name;
         this.grade = grade;
         this.age = age;
+    }*/
+
+    //Methods
+    public int total(){
+        return mark_1+mark_2+mark_3;
+    }
+    public double average(){
+        return (float)total()/3;
+    }
+    public String grade(){
+        if(average() >= 60)
+            return "Passed A";
+        else
+            return "Failed B";
+    }
+    public String details(){
+        return "\nRoll no: " + roll + "\nName: " + name + "\nCourse: " + course + "\n";
     }
 
+
     public static void main(String[] args) {
-        // Student Challenge - Write a Class 1
-        StudentChallengeWriteClass obj = new StudentChallengeWriteClass("Baby", "Grade 9", 30 );
-        StudentChallengeWriteClass obj2 = new StudentChallengeWriteClass("Khensani", "Grade 10", 14);
+        StudentChallengeWriteClass obj = new StudentChallengeWriteClass();
 
-        System.out.println("First Student: "+ obj.name);
-        System.out.println("First Student: "+ obj.grade);
-        System.out.println("First Student: "+ obj.age);
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Second Student: " + obj2.name);
-        System.out.println("Second Student: " + obj2.grade);
-        System.out.println("Second Student: " + obj2.age);
+        System.out.print("Enter roll: ");
+        obj.roll = sc.nextInt();
+
+        sc.nextLine(); //clearing leftover
+        System.out.print("Enter name: ");
+        obj.name = sc.nextLine();
+
+        System.out.print("Enter course: ");
+        obj.course = sc.nextLine();
+
+        System.out.print("Enter mark 1: ");
+        obj.mark_1 = sc.nextInt();
+
+        System.out.print("Enter mark 2: ");
+        obj.mark_2 = sc.nextInt();
+
+        System.out.print("Enter mark 3: ");
+        obj.mark_3 = sc.nextInt();
+
+        System.out.println();
+        System.out.println("Total Score: " + obj.total());
+        System.out.printf("Average: %.2f%n", obj.average());
+        System.out.println("Student details: " + obj.details());
+
+        sc.close();
     }
 }
