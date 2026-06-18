3f93b0f4f295a15d3e2c615c904aabea78bb8be1
‘Kalophain14’ <chueutemosho@gmail.com>
2026-06-18 20:07:58 +0200

    practicing creating a cclass of a cylinder and calculating all its circumference + area + volume and totalsurface

--- java/11-object-oriented-programming/StudentChallengeWriteClass3.java
+++ java/11-object-oriented-programming/StudentChallengeWriteClass3.java
@@ -1,6 +1,32 @@
 public class StudentChallengeWriteClass3 {
 
+    //Class Cylinder
+
+    //properties
+    double radius;
+    double height;
+
+    public double lidArea(){
+        return Math.PI * radius * radius;
+    }
+    public double circumference(){
+        return 2 * Math.PI*radius;
+    }
+    public double totalSurfaceArea(){
+        return 2 * lidArea() + circumference() * height;
+    }
+    public double volume(){
+        return lidArea()*height;
+    }
+
     public static void main(String[] args) {
-        // Student Challenge - Write a Class 3
+        StudentChallengeWriteClass3 obj = new StudentChallengeWriteClass3();
+
+        obj.radius = 10;
+        obj.height = 7;
+
+        System.out.println("lidArea is: " + obj.lidArea());
+        System.out.println("Surface is: " + obj.totalSurfaceArea());
+        System.out.println("Volume is: " + obj.volume());
     }
 }
