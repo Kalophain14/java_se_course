64f73c78f6c3b73451830b7290f13a36193ad84a
‘Kalophain14’ <chueutemosho@gmail.com>
2026-06-18 19:19:21 +0200

    Practicing creating a class of rectangle that has its two properties and two methods that calculate area + perimeter

--- java/11-object-oriented-programming/StudentChallengeWriteClass2.java
+++ java/11-object-oriented-programming/StudentChallengeWriteClass2.java
@@ -1,6 +1,33 @@
 public class StudentChallengeWriteClass2 {
 
+    //properties
+    double length;
+    double width;
+
+    //Methods
+    public double area(){
+        return length * width;
+    }
+
+    public double perimeter(){
+        return  2 * (length + width);
+    }
+
     public static void main(String[] args) {
-        // Student Challenge - Write a Class 2
+        // Student Challenge - Write a Class Rectangle
+        StudentChallengeWriteClass2 obj = new StudentChallengeWriteClass2();
+        StudentChallengeWriteClass2 obj2 = new StudentChallengeWriteClass2();
+
+        obj.length = 10;
+        obj.width = 12;
+
+        obj2.length = 4;
+        obj2.width = 10;
+
+        System.out.println("The area of a rectangle: " + obj.area());
+        System.out.println("The area of a rectangle: " + obj.perimeter());
+
+        System.out.println("The area of a rectangle: " + obj2.area());
+        System.out.println("The area of a rectangle: " + obj2.perimeter());
     }
 }
