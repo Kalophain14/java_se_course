68921de79bcb0e48e073abfc5c802f6f73208211
‘Kalophain14’ <chueutemosho@gmail.com>
2026-06-20 00:04:16 +0200

    Types of properties, read and write, read only and write only how each uses the set and get method to have user access certain properties

--- java/11-object-oriented-programming/TypesOfProperties.java
+++ java/11-object-oriented-programming/TypesOfProperties.java
@@ -1,6 +1,20 @@
 public class TypesOfProperties {
 
+    /*
+    Types of properties
+
+    1. Read and Writeable = it is used when you can read
+                          = write the properties (get & set are used)
+
+    2. Read only = get method allowed you cant change the value
+                 = an example is an account number it cant be changed
+
+    3. Write only = set method allowed you cant change the properties
+                  = Produce (set) & consumer (get)
+                  = Paying an account, you cant see the value of the balance after payment
+                  = You can see acc, name and owner of the account no
+     */
+
     public static void main(String[] args) {
-        // Types of Properties
     }
 }
