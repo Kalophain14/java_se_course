bdd37d6c47058fdd6d13b00f7c15f69de99aa44e
‘Kalophain14’ <chueutemosho@gmail.com>
2026-06-19 23:30:20 +0200

    Data hiding its used to hide the important data whilst letting the user access the operators,hiding the data we use private keyword, therefore the data is only accessible by that class and the methods inside it then to access it outside  we use get(READ DATA) and set(WRITE DATA) to access those data

--- java/11-object-oriented-programming/DataHiding.java
+++ java/11-object-oriented-programming/DataHiding.java
@@ -1,6 +1,61 @@
-public class DataHiding {
+/*
+Data Hiding
+= Data is hidden eg vehicle engine, TV circuit board
+= Operators uses the data hidden to be able to run the method
+= Operators comes as TV button, gears, and steering wheel
+*/
+
+
+class DataHiding {
+
+    //Data Hidden Properties
+    private int length;
+    private int width;
+
+    /*
+    Accessing Private Data using
+    = get(READ)
+    = set(WRITE)
+     */
+
+    //Length
+    int getLength(){
+        return length;
+    }
+    void setLength(int l){
+        if(l > 0)
+            length = l;
+        else
+            length = 0;
+    }
+
+    //Width
+    int getWidth(){
+        return width;
+    }
+    void setWidth(int w){
+        if(w > 0)
+            width = w;
+        else
+            width = 0;
+    }
+
+    //Methods Using The Data
+    public int area(){
+        return length * width;
+    }
+    public int perimeter(){
+        return 2*(length + width);
+    }
 
     public static void main(String[] args) {
-        // Data Hiding
+        DataHiding obj = new DataHiding();
+
+        //Setting Properties
+        obj.setLength(10);
+        obj.setWidth(5);
+
+        System.out.println(obj.area());
+        System.out.println(obj.perimeter());
     }
 }
