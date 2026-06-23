e246d784e4f0834c512e4de681002bfac9223397
‘Kalophain14’ <chueutemosho@gmail.com>
2026-06-23 21:55:18 +0200

    constructors + classes

--- java/11-object-oriented-programming/TypesOfProperties.java
+++ java/11-object-oriented-programming/TypesOfProperties.java
@@ -1,20 +1,35 @@
 public class TypesOfProperties {
 
     /*
-    Types of properties
-
-    1. Read and Writeable = it is used when you can read
-                          = write the properties (get & set are used)
-
-    2. Read only = get method allowed you cant change the value
-                 = an example is an account number it cant be changed
-
-    3. Write only = set method allowed you cant change the properties
-                  = Produce (set) & consumer (get)
-                  = Paying an account, you cant see the value of the balance after payment
-                  = You can see acc, name and owner of the account no
+     * Types of properties
+     * 
+     * 1. Read and Writeable = it is used when you can read
+     * = write the properties (get & set are used)
+     * 
+     * 2. Read only = get method allowed you cant change the value
+     * = an example is an account number it cant be changed
+     * 
+     * 3. Write only = set method allowed you cant change the properties
+     * = Produce (set) & consumer (get)
+     * = Paying an account, you cant see the value of the balance after payment
+     * = You can see acc, name and owner of the account no
      */
 
+    String name;
+    int age;
+    boolean isEnrolled;
+
+    public TypesOfProperties(String name, int age, boolean isEnrolled) {
+
+        this.age = age;
+        this.name = name;
+        this.isEnrolled = isEnrolled;
+    }
+
     public static void main(String[] args) {
+
+        TypesOfProperties obj = new TypesOfProperties("John", 20, true);
+
+        System.out.println("Student name: " + obj.name + " Student age: " + obj.age);
     }
 }
