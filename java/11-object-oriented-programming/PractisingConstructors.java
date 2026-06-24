a694df6165e67ee7a688537f4ee4e1ebb75a8952
‘Kalophain14’ <chueutemosho@gmail.com>
2026-06-24 16:52:40 +0200

    creating a class that takes products details, the name + itemNo dont change, we dont set them, we only get the value from the constructor as we use the class, we set and get the price and quantity as they channge

--- java/11-object-oriented-programming/PractisingConstructors.java
+++ java/11-object-oriented-programming/PractisingConstructors.java
@@ -1,6 +1,60 @@
 public class PractisingConstructors {
 
+    //Product properties
+    private String itemNo;
+    private String name;
+    private double price;
+    private double qty;
+
+    //Getters
+    public String getItemNo() {
+        return itemNo;
+    }
+    public String name() {
+        return name;
+    }
+    public double price() {
+        return price;
+    }
+    public double qty() {
+        return qty;
+    }
+
+    /*
+    Setters
+
+    Since the itemNo & name of the products
+    dont change no need to set them
+    */
+    public void setPrice(double price) {
+        if (price < 0) {
+            System.out.println("Invalid price");
+        } else  {
+            this.price = price;
+        }
+    }
+    public void setQty(double qty) {
+       if (qty < 0) {
+           System.out.println("Invalid quantity");
+       } else   {
+           this.qty = qty;
+       }
+    }
+
+    //Constructor
+    public PractisingConstructors(String itemNo, String name, double price, double qty) {
+        this.itemNo = itemNo;
+        this.name = name;
+        this.price = price;
+        this.qty = qty;
+    }
+
     public static void main(String[] args) {
-        // Practising Constructors
+
+        PractisingConstructors obj = new PractisingConstructors("A124", "TV", 1200.92, 2.0);
+        System.out.println("Name : " + obj.name());
+        System.out.println("Price : " + obj.price());
+        System.out.println("Quantity : " + obj.qty());
+
     }
 }
