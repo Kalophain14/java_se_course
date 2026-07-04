23494cb25c1ffb41423369980a1307e668032a08
‘Kalophain14’ <chueutemosho@gmail.com>
2026-07-04 21:54:37 +0200

    learned about the different between a paramtised constructor + non parameter class + how to call the parent class aka super class, ot has to always be the firstline not second

--- java/12-inheritance/ConstructorsInInheritance.java
+++ java/12-inheritance/ConstructorsInInheritance.java
@@ -1,6 +1,35 @@
 public class ConstructorsInInheritance {
 
+
+    public static class Parent
+    {
+        public Parent()
+        {
+            System.out.println("Parent Constructor");
+        }
+    }
+
+    public static class Child extends Parent
+    {
+        public Child()
+        {
+            System.out.println("Child Constructor");
+        }
+    }
+
+    public static class Grandchild extends Child
+    {
+        public Grandchild()
+        {
+            System.out.println("Grandchild Constructor");
+        }
+    }
+
     public static void main(String[] args) {
         // Constructors in Inheritance
+
+        //Parent obj = new Parent();
+        //Child child = new Child();
+        Grandchild grandchild = new Grandchild();
     }
 }
--- java/12-inheritance/ParameterisedConstructors.java
+++ java/12-inheritance/ParameterisedConstructors.java
@@ -1,6 +1,53 @@
 public class ParameterisedConstructors {
 
+
+    public static class Parent
+    {
+        Parent()
+        {
+            System.out.println("Non Parameter Parent");
+        }
+
+        Parent(int x)
+        {
+            System.out.println("Parameter Parent Takes: " + x);
+        }
+    }
+
+    public static class Child extends Parent
+    {
+        Child()
+        {
+            System.out.println("Non Parameter Child");
+        }
+
+        Child(int y)
+        {
+            System.out.println("Parameter Child");
+        }
+
+        Child(int x, int y)
+        {
+            super(x);
+            System.out.println("2 Parameter Child");
+        }
+    }
+
+    public static class Grandchild extends Child
+    {
+        Grandchild()
+        {
+            System.out.println("Non Parameter Grandchild");
+        }
+        Grandchild(int z)
+        {
+            System.out.println("Parameter Grandchild");
+        }
+    }
+
     public static void main(String[] args) {
-        // Parameterised Constructors
+        // Constructors in Inheritance
+
+        Child child = new Child(21, 20);
     }
 }
