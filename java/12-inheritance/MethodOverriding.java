137efc316b2f11ea96da195e31744f67584793bd
‘Kalophain14’ <chueutemosho@gmail.com>
2026-07-16 21:34:58 +0200

    Overriding classes using the sub class, you can re-define the method inside the super class, and mostly importantly the object of a method will always be called rather than the reference when creating an object SuperClass test = new SubClass the subclass method will be called thus overrding the super class

--- java/12-inheritance/MethodOverriding.java
+++ java/12-inheritance/MethodOverriding.java
@@ -1,6 +1,42 @@
-public class MethodOverriding {
+package MethodOverriding;
 
-    public static void main(String[] args) {
-        // Method Overriding
+    /*
+    Method overriding = it is when you re-define
+                      = the method in the super class(parent) in the sub class(child)
+    Why method overriding
+    */
+
+    class Super{
+        public void display()
+        {
+            System.out.println("Super Class");
+        }
     }
-}
+
+    class Sub extends Super{
+        @Override
+        public void display()
+        {
+            System.out.println("Sub Class borrowed");
+        }
+    }
+
+    public class MethodOverriding
+    {
+        public static void main (String[]args){
+        // Method Overriding
+        Super m = new Super();
+        m.display();
+
+        Sub c = new Sub();
+        c.display();
+
+        /*
+        NB The method will always be called depending
+        on the object not reference
+         */
+        Super su = new Sub(); // Sub class will be called
+        su.display();
+
+        }
+    }
