5126f3c15f1ea2c944c0394f382772a5797f4e05
‘Kalophain14’ <chueutemosho@gmail.com>
2026-07-31 23:20:18 +0200

    Is when you are using the reference of a superclass  although dispatching the subclass as an objectf Method are called depending on the object not the reference  It is called Runtime polymorphism

--- java/12-inheritance/DynamicMethodDispatch.java
+++ java/12-inheritance/DynamicMethodDispatch.java
@@ -1,4 +1,21 @@
-public class DynamicMethodDispatch {
+package DynamicMethodDispatch;
+
+/*
+ Dyamic Method Dispacth
+
+ Is when you are using the reference of a superclass
+ although dispatching the subclass as an object
+
+ Reference          Object
+ SuperClass s = new SubClass();
+
+ Method are called depending on the object not the reference
+ It is called Runtime polymorphism
+*/
+
+
+
+public class DynamicMethodDispatch{
 
     public static void main(String[] args) {
         // Dynamic Method Dispatch
