8770d7e01a8643b1029a1b858522acc7e90d3e75
‘Kalophain14’ <chueutemosho@gmail.com>
2026-07-31 22:49:53 +0200

    Method Overriding, you can always Reference by SuperClass and access using SubClass
    Tv OldTv = new SmartTV();
    
    although you will have access only to the methods inside the the TV SuperClass

--- java/12-inheritance/ExampleMethodOverriding2.java
+++ java/12-inheritance/ExampleMethodOverriding2.java
@@ -1,6 +1,37 @@
-public class ExampleMethodOverriding2 {
+package ExampleMethodOverriding2;
 
-    public static void main(String[] args) {
-        // Example 2 - Method Overriding
-    }
+
+//SuperClass
+class Vehicle {
+    public void start() { System.out.println("Vehicle starting"); }
+    public void stop() { System.out.println("Vehicle stopping"); }
+    public void changeGear() { System.out.println("Vehicle uses manual gear"); }
 }
+
+//Sub Class Extends and Override
+class Vehicle2 extends Vehicle {
+    @Override
+    public void start() { System.out.println("Vehicle2 starting");}
+    public void stop() { System.out.println("Vehicle2 stopping");}
+    public void changeGear() { System.out.println("Vehicle2 uses an automatic gear");}
+    // new Method from SubClass Vehicle2
+    public void openRoof() { System.out.println("Vehicle2 has a open roof"); }
+    }
+
+    public class ExampleMethodOverriding2 {
+        public static void main(String[] args) {
+            System.out.println("== SuperClass ==");
+            Vehicle BmwM3 = new Vehicle();
+            BmwM3.start();
+            BmwM3.stop();
+            BmwM3.changeGear();
+
+            System.out.println("== Sub Class ==");
+            Vehicle2 BmwM4 = new Vehicle2();
+            BmwM4.start();
+            BmwM4.stop();
+            BmwM4.changeGear();
+            BmwM4.openRoof();
+
+        }
+    }
