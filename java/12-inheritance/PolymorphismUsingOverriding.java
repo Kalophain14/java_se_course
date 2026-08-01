e9bc74f245882a4cf2690e9a6ce020c996661f27
‘Kalophain14’ <chueutemosho@gmail.com>
2026-08-01 21:38:22 +0200

    Polymorphism - It means one name, different actions Poly = Many Morphism = Forms

--- java/12-inheritance/PolymorphismUsingOverriding.java
+++ java/12-inheritance/PolymorphismUsingOverriding.java
@@ -1,4 +1,22 @@
-public class PolymorphismUsingOverriding {
+package PolymorphismUsingOverriding;
+
+
+/*
+Polymorphism - It means one name, different actions
+Poly = Many
+Morphism = Forms
+
+It is achieved using method Overloading and Overriding
+ */
+
+class
+
+
+
+
+
+
+class PolymorphismUsingOverriding{
 
     public static void main(String[] args) {
         // Polymorphism using Overriding
