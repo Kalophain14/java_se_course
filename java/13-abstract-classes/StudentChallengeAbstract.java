ece5e7766aa0ccb233a8630110752953743d21a8
‘Kalophain14’ <chueutemosho@gmail.com>
2026-08-09 18:48:48 +0200

    Shape
    Build a mini project that has a standard Shape Abstract Class that has 2 Abstract classes, which is extended from the Circle and Rectangle both calculating the area and perimiters methods overrided from the abstract class and then used Polymorphism calling using the Reference of the abstract class and the object as the extended sub classes

--- java/13-abstract-classes/StudentChallengeAbstract.java
+++ java/13-abstract-classes/StudentChallengeAbstract.java
@@ -1,6 +1,64 @@
-public class StudentChallengeAbstract {
+package StudentChallangeAbstract;
 
+abstract class Shape {
+    abstract double perimeter();
+    abstract double area();
+}
+
+class Circle extends Shape {
+    double radius;
+
+    // Constructors Access
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    // Methods override
+    @Override
+    public double perimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle extends Shape {
+    double length;
+    double breadth;
+
+    // Constructors Access
+    Rectangle(double length, double breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    // Methods override
+    @Override
+    public double perimeter() {
+        return 2 * (length + breadth);
+    }
+
+    @Override
+    public double area() {
+        return length * breadth;
+    }
+}
+
+public class StudentChallengeAbstract {
     public static void main(String[] args) {
-        // Student Challenge - Abstract Class
+        // Polymorphism Calling
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(4, 6);
+
+        System.out.println("===Circle===");
+        System.out.println("Perimeter: " + circle.perimeter());
+        System.out.println("Area: " + circle.area());
+
+        System.out.println("===Rectangle===");
+        System.out.println("Perimeter: " + rectangle.perimeter());
+        System.out.println("Area: " + rectangle.area());
     }
 }
