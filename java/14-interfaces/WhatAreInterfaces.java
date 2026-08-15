a537e2f955773b955ee9d60c3fb57fd1d59c4ac0
‘Kalophain14’ <chueutemosho@gmail.com>
2026-08-15 22:59:13 +0200

    Interfaces
    Interfaces are like abstract classes although instead of extending to the superclass, they implement, if they don't implement the class becomes an abstract class, they use the keyword *Implement* to attribute themselves

--- java/14-interfaces/WhatAreInterfaces.java
+++ java/14-interfaces/WhatAreInterfaces.java
@@ -1,6 +1,44 @@
+package WhatAreInterfaces;
+
+/*
+Interface is used to achieve only Polymorphism
+Interfaces are abstract, you can't create an object of the reference
+You can only create the reference of the interface class
+The subclasses don't extend rather implements to the superClass
+ */
+
+interface SuperTest1{
+    void meth1();
+    void meth2();
+}
+
+class SubTest2 implements SuperTest1{
+    public void meth1(){
+        System.out.println("meth1 Override");
+    }
+    public void meth2(){
+        System.out.println("meth2 Override");
+    }
+
+    public void meth3(){
+        System.out.println("New Method from SubClass");
+    }
+}
+
 public class WhatAreInterfaces {
 
     public static void main(String[] args) {
-        // What are Interfaces
+        // Cant call 3rd method as you are using the reference of superclass
+        System.out.println("==Reference Superclass & Object Subclass==");
+        SuperTest1 test1 = new SubTest2();
+        test1.meth1();
+        test1.meth2();
+        System.out.println();
+        // Reference and Object same class
+        System.out.println("==Reference and Object Subclass==");
+        SubTest2 test2 = new SubTest2();
+        test2.meth1();
+        test2.meth2();
+        test2.meth3();
     }
 }
