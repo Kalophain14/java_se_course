4100a845d7bb4d04dc66365f1011deeadf9655a9
‘Kalophain14’ <chueutemosho@gmail.com>
2026-08-18 21:59:47 +0200

    Interfaces
    Abstract/regular classes lets you extend ones and use the methods(behaviours) once, if you want to add more behaviours, it becomes difficult that is where interfaces comes in they are there to allow you to add more behaviours to the class you already have, interfaces are abstract class by nature, they need to be override and they can also include a default method with an actual body as well
    
    interface(); // no body abstract
    interface{
    default void LogDefault(){
    sout(This

--- java/15-inner-classes/StaticInnerClass.java
+++ java/15-inner-classes/StaticInnerClass.java
@@ -1,6 +1,28 @@
+interface Test1{
+    void meth1();
+    void meth2();
+}
+
+class Test2 implements Test1{
+    //override abstract interference class
+    @Override
+    public void meth1() {
+        System.out.println("Hello");
+    }
+
+    @Override
+    public void meth2() {
+        System.out.println("Hey you");
+    }
+}
+
 public class StaticInnerClass {
 
     public static void main(String[] args) {
         // Static Inner Class
+
+        Test2 t2 = new Test2();
+        t2.meth1();
+        t2.meth2();
     }
 }
