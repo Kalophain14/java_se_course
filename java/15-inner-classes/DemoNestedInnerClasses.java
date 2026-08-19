a3cc5f61123a3ef3aa2ad243ed3f8c83465b45d1
‘Kalophain14’ <chueutemosho@gmail.com>
2026-08-19 23:04:20 +0200

    Local and Anonymous class
    Local Classes: They are stored inside a method and have to be called inside that method
    
    Anonymous classes: They are created from an abstract class by using its object, wrapping and overriding the abstract class and its method creating a concrete class, they become nameless, because they are created from an abstract class objects

--- java/15-inner-classes/DemoNestedInnerClasses.java
+++ java/15-inner-classes/DemoNestedInnerClasses.java
@@ -1,3 +1,20 @@
+package DemoNestedInnerClasses;
+
+class Outer{
+
+    void Display(){
+        // Local Class Inner
+        // A class within a method
+        class localInner{
+            void InnerDisplay(){
+                System.out.println("Hello");
+            }
+        }
+        localInner i = new localInner();
+        i.InnerDisplay();
+    }
+}
+
 public class DemoNestedInnerClasses {
 
     public static void main(String[] args) {
--- java/15-inner-classes/LocalAndAnonymousInner.java
+++ java/15-inner-classes/LocalAndAnonymousInner.java
@@ -1,6 +1,46 @@
-public class LocalAndAnonymousInner {
+package LocalAndAnonymousInner;
+
+//LocalInnerClass
+class Outer{
+
+    void Display(){
+        // A class within a method
+        class localInner{
+            void InnerDisplay(){
+                System.out.println("Hello");
+            }
+        }
+        localInner i = new localInner();
+        i.InnerDisplay();
+    }
+}
+
+
+// Anonymous Class
+
+abstract class My{
+    abstract void display();
+}
+
+class AnonymousOuter{
+    public void AnonymousMeth(){
+        // The objects of the abstract class becomes a class
+        // then overriding the method inside the class
+        // therefore becomes anonymous class
+        My m = new My() {
+            @Override
+            void display()
+            {
+                System.out.println("Hello");
+            }
+        };
+        m.display();
+    }
+}
+
+public class LocalAndAnonymousInner; {
 
     public static void main(String[] args) {
-        // Local and Anonymous Inner Classes
+        // Demo - Nested Inner Classes
     }
 }
