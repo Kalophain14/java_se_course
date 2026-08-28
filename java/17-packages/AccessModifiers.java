acb7b6e4ed516dc9dca6a6a2fca0c1ca0cdf8e86
‘Kalophain14’ <chueutemosho@gmail.com>
2026-08-28 20:26:01 +0200

    Access Modifiers Introduction

--- java/17-packages/AccessModifiers.java
+++ java/17-packages/AccessModifiers.java
@@ -1,3 +1,13 @@
+/*
+A Class Can have 3 Things:
+- Variables
+- Method, Interfaces
+- Inner Classes
+
+Access Modifiers =
+
+ */
+
 public class AccessModifiers {
 
     public static void main(String[] args) {
