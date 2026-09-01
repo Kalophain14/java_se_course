911f4d861fd10fa5177c1d8ee6e5cf1a166cb19f
‘Kalophain14’ <chueutemosho@gmail.com>
2026-09-01 22:39:32 +0200

    Packages are like folders - They are used to store re-use code that is within a package - On the terminal javac -d packagename.java creates package folder - Creating classes for a package

--- java/17-packages/CreatingAPackage2.java
+++ java/17-packages/CreatingAPackage2.java
@@ -1,27 +1,27 @@
-package mypack1.inner;
+package mypack1;
 
-class Package {
+class Package2 {
     // Properties
     public String name;
     public int price;
 
     // Constructors
-    public Package (String name, int price) {
+    public Package2 (String name, int price) {
         this.name = name;
         this.price = price;
     }
 
     // Method
-    public void Package () {
+    public void Package2 () {
         System.out.println("Package " + this.name + ", " + this.price);
     }
 }
 
-public class CreatingAPackage {
+public class CreatingAPackage2 {
 
     public static void main(String[] args) {
         // Creating a Package
-        Package p1 = new Package("P1", 100);
-        p1.Package ();
+        Package2 p1 = new Package2("P1", 500);
+        p1.Package2 ();
     }
 }
--- java/17-packages/DemoAccessModifiers.java
+++
@@ -1,6 +0,0 @@
-public class DemoAccessModifiers {
-
-    public static void main(String[] args) {
-        // Demo - Access Modifiers
-    }
-}
